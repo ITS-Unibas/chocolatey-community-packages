9f5a6de188a0391bcfebf45987cb44505dc32730
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-09 10:51:07 +0200

    Adds atlassian-companion

---
+++ automatic/atlassian-companion/README.md
@@ -0,0 +1,5 @@
+The Atlassian Companion app enables users to edit Confluence files in their preferred desktop application, then save the file back to Confluence automatically.
+
+The download and re-upload of files is managed by the Atlassian Companion app, which needs to be installed on each user's machine (not in the Confluence installation directory) to enable file editing.
+
+![screenshot](https://cdn.jsdelivr.net/gh/ITS-Unibas/chocolatey-community-packages@master/automatic/atlassian-companion/screenshot.png)
---
+++ automatic/atlassian-companion/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'atlassian-companion'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url            = ''
+
+  #MSI
+  silentArgs     = "/qn /norestart /l*v `"$($env:SWP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0, 3010, 1641)
+  #OTHERS
+  softwareName   = 'atlassian-companion*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum       = ''
+  checksumType   = 'sha256' #default is md5, can also be sha1
+}
+
+Install-ChocolateyPackage @packageArgs
