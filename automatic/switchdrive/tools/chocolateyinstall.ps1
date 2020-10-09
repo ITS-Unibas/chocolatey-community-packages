f1ea601ec95c2328b21756be5815490989e3105e
Chocolatey <chocolatey@realdimensions.net>
2020-10-09 07:55:28 +0000

    AU: 1 updated - switchdrive [skip ci] https://gist.github.com/7ccfa9c48ba8a91d5e70190fe1b85e0d/964f2faa0f78304c69d5318c3c8ae3e73c1e88b7

--- automatic/switchdrive/tools/chocolateyinstall.ps1
+++ automatic/switchdrive/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'msi'
-  url            = ''
+  url            = 'https://misc.www.switch.ch/drive/SWITCHdrive-2_5_4_1832.msi'
   silentArgs     = 'MSIRESTARTMANAGERCONTROL=Disable ALLUSERS=1 REBOOT=ReallySuppress /qn'
   validExitCodes = @(0)
 
   softwareName   = 'SWITCHdrive*'
-  checksum       = ''
+  checksum       = '442f452c4ddcc7f7bc81eb9a292ce509cc3637d4f313dbd5e6b8565825f745b9'
   checksumType   = 'sha256'
 }
 
