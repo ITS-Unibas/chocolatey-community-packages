5976c8b109107b03b822ce44d6227ef443f95eb3
Chocolatey <chocolatey@realdimensions.net>
2020-10-09 15:04:41 +0000

    AU: 1 updated - polar-flowsync [skip ci] https://gist.github.com/7ccfa9c48ba8a91d5e70190fe1b85e0d/be1cac8621e901ec4fa1733c39e6e7cff9da252b

--- automatic/polar-flowsync/tools/chocolateyinstall.ps1
+++ automatic/polar-flowsync/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = ''
+  url            = 'https://flowsync.cdn.polar.com/connect/download/FlowSync_3.0.0.1337.exe'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'Polar FlowSync*'
-  checksum       = ''
+  checksum       = '404631183cb271e45bca369a11103a012c44718e94ca701d4a4976111176513c'
   checksumType   = 'sha256'
 }
 
