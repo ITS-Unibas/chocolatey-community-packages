475d5fa92947246c107b3d9a3fba46a623c5ca8c
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-09 09:54:33 +0200

    Adds switchdrive

---
+++ automatic/switchdrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'switchdrive'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  url            = ''
+  silentArgs     = 'MSIRESTARTMANAGERCONTROL=Disable ALLUSERS=1 REBOOT=ReallySuppress /qn'
+  validExitCodes = @(0)
+
+  softwareName   = 'SWITCHdrive*'
+  checksum       = ''
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
