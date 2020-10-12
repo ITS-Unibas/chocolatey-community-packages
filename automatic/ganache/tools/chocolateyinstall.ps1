c229a8188d7bdd8c10f2ebc4d82b351591b56a82
Chocolatey <chocolatey@realdimensions.net>
2020-10-12 13:21:56 +0000

    AU: 1 updated - ganache [skip ci] https://gist.github.com/7ccfa9c48ba8a91d5e70190fe1b85e0d

--- automatic/ganache/tools/chocolateyinstall.ps1
+++ automatic/ganache/tools/chocolateyinstall.ps1
@@ -9,10 +9,10 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = ''
+  url64bit       = 'https://github.com//trufflesuite/ganache/releases/download/v2.4.0/Ganache-2.4.0-win-setup.exe'
   silentArgs     = '/S /ALLUSERS=1'
   softwareName   = 'Ganache*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum64     = ''
+  checksum64     = '8725b1c4d99baf57e0a82cb3ae0bbfeb85345991281550f64f3ab4a04389c91f'
   checksumType64 = 'sha256' #default is checksumType
 }
 
