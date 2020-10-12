c8b28f024c7792b2c5524751eb66161f0dce89e6
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-12 15:17:10 +0200

    Adds ganache

---
+++ automatic/ganache/README.md
@@ -0,0 +1,3 @@
+Ganache is a personal blockchain for rapid Ethereum and Corda distributed application development. You can use Ganache across the entire development cycle; enabling you to develop, deploy, and test your dApps in a safe and deterministic environment.
+
+![screenshot](https://raw.githubusercontent.com/trufflesuite/ganache/develop/.github/images/ganache_screenshot.jpg)
---
+++ automatic/ganache/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName = 'ganache'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url64bit       = ''
+  silentArgs     = '/S /ALLUSERS=1'
+  softwareName   = 'Ganache*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum64     = ''
+  checksumType64 = 'sha256' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
