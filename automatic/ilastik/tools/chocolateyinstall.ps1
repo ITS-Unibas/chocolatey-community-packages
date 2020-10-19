41430d1ecf3a7c3daf9c87eff08803d6234d9ee4
Maximilian Burgert <max.burgert@unibas.ch>
2020-10-19 10:21:00 +0200

    Adds ilastik

---
+++ automatic/ilastik/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'ilastik'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url64bit       = ''
+  silentArgs     = '/VERYSILENT /NORESTART /NOCANCEL'
+  validExitCodes = @(0)
+  softwareName   = 'ilastik*'
+  checksum64     = ''
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+# Set icon in appwiz
+$ilastikFolder = Get-ChildItem "C:\Program Files\" | Where-Object { $_.PSIsContainer -eq $true -and $_.Name -match "ilastik" } | ForEach-Object { $_.FullName }
+New-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\ilastik_is1' -Name 'DisplayIcon' -PropertyType String -Value "$ilastikFolder\ilastik.exe" -Force -ErrorAction SilentlyContinue
---
+++ automatic/ilastik/update.ps1
@@ -0,0 +1,30 @@
+﻿Import-Module AU
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://www.ilastik.org/download.html'
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge -FileNameBase 'ilastik'
+  $Latest.Checksum = Get-RemoteChecksum $Latest.URL -Algorithm 'sha256'
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1`'$($Latest.URL)`'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1`'$($Latest.Checksum)`'"
+    }
+  }
+}
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $regex = '.exe$'
+  $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+  $arr = $url -split '-|.exe'
+  $version = $arr[1]
+  if ($version -match "post") {
+    $version = $version.replace("post",".")
+  }
+  return @{ Version = $version; URL = $url }
+}
+
+update -ChecksumFor none -NoCheckChocoVersion
