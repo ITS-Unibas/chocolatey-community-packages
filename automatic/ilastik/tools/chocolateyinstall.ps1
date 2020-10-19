1d49a6f2a5c2bd90fbe37e56fd4b940207bd403e
Chocolatey <chocolatey@realdimensions.net>
2020-10-19 08:25:31 +0000

    AU: 1 updated - ilastik [skip ci] https://gist.github.com/7ccfa9c48ba8a91d5e70190fe1b85e0d

--- automatic/ilastik/tools/chocolateyinstall.ps1
+++ automatic/ilastik/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = ''
+  url64bit       = 'https://files.ilastik.org/ilastik-1.3.3post3-win64.exe'
   silentArgs     = '/VERYSILENT /NORESTART /NOCANCEL'
   validExitCodes = @(0)
   softwareName   = 'ilastik*'
-  checksum64     = ''
+  checksum64     = 'af3495be3236ef6837dcc7a84ecd4690c184fd13c7a7b585431837e1b2c8cd6f'
   checksumType64 = 'sha256'
 }
 
