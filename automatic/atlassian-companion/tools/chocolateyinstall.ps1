102396d0a215ce85201f041784505f0158608087
Chocolatey <chocolatey@realdimensions.net>
2020-11-22 00:05:11 +0000

    AU: 1 updated - atlassian-companion [skip ci] https://gist.github.com/7ccfa9c48ba8a91d5e70190fe1b85e0d

--- automatic/atlassian-companion/tools/chocolateyinstall.ps1
+++ automatic/atlassian-companion/tools/chocolateyinstall.ps1
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes = @(0, 3010, 1641)
   #OTHERS
   softwareName   = 'atlassian-companion*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '6a7e2ff088e1385da0030e133e59537da87d02aa6ece7fa1b91fda3938b62a0d'
+  checksum       = 'aad81759e49745b77e7fee702b376729fcd5720db5f40f7932b21869c16dcf37'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
