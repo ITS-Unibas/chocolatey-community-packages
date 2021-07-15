b2c5a7c53c643ef8136e74d5db91f8093536d35a
Chocolatey <chocolatey@realdimensions.net>
2021-07-15 00:28:06 +0000

    AU: 1 updated - cellprofiler-analyst [skip ci] https://gist.github.com/7ccfa9c48ba8a91d5e70190fe1b85e0d/0ead52787f513458746c1eea071799ee42041282

--- automatic/cellprofiler-analyst/tools/chocolateyinstall.ps1
+++ automatic/cellprofiler-analyst/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://cellprofiler-releases.s3.amazonaws.com/CellProfiler-Analyst-Windows-3.0.2.exe'
+  url            = 'https://cellprofiler-releases.s3.amazonaws.com/CellProfiler-Analyst-Windows-3.0.3.exe'
   silentArgs     = '/VERYSILENT /NORESTART /NOCANCEL'
   validExitCodes = @(0)
   softwareName   = 'Cellprofiler-Analyst*'
-  checksum       = '477a5f598695659b83d22af62dd86fa31040302d521f165a0040452d4fff6ab7'
+  checksum       = '0c9b884b640aafbc335ce3c2842740efa72b0f4db9964b2ecdcf20378c240bed'
   checksumType   = 'sha256'
 }
 
