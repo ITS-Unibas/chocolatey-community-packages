2435e2351f5d1e0ba98193188fe9c115cafa2344
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2022-01-25 08:36:44 +0100

    URL-Fix

--- automatic/ganache/tools/chocolateyinstall.ps1
+++ automatic/ganache/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://github.com/trufflesuite/ganache/releases/download/v2.5.4/Ganache-2.5.4-win-setup.exe'
+  url64bit       = 'https://github.com/trufflesuite/ganache-ui/releases/download/v2.5.4/Ganache-2.5.4-win-setup.exe'
   silentArgs     = '/S /ALLUSERS=1'
   softwareName   = 'Ganache*'
   checksum64     = 'b7e4ec85691aabcc57156c15dddbb0ecba46a71e5507cb6ac673cf04874e9748'
