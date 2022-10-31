707fc2355e8c91e313bb65263e67b46a33de309d
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2022-10-31 15:34:11 +0100

    Update update_all.ps1

--- update_all.ps1
+++ update_all.ps1
@@ -81,7 +81,7 @@ $Options = [ordered]@{
             EnableSsl   = $Env:mail_enablessl -eq 'true'
             Attachment  = "$PSScriptRoot\update_info.xml"
             UserMessage = ''
-            SendAlways  = $false                        #Send notifications every time
+            SendAlways  = $true                        #Send notifications every time
         }
     }
     else {}
