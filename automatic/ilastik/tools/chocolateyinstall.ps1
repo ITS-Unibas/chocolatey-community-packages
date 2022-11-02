a5fb74c01b33ca83368034d8700fb483b0fe09ed
ITS-Unibas <ITS-Unibas-github@gmail.com>
2022-11-02 10:25:37 +0000

    AU: 1 updated - ilastik [skip ci]

--- automatic/ilastik/tools/chocolateyinstall.ps1
+++ automatic/ilastik/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url64bit       = 'https://files.ilastik.org/ilastik-1.3.3post3-win64.exe'
+  url64bit       = 'https://files.ilastik.org/ilastik-1.4.0rc6-win64.exe'
   silentArgs     = '/VERYSILENT /NORESTART /NOCANCEL'
   validExitCodes = @(0)
   softwareName   = 'ilastik*'
-  checksum64     = 'af3495be3236ef6837dcc7a84ecd4690c184fd13c7a7b585431837e1b2c8cd6f'
+  checksum64     = '95002abf7eef161bba6cee7b2f03fc1f8caa33fa09dec627647c43cf884312d7'
   checksumType64 = 'sha256'
 }
 
