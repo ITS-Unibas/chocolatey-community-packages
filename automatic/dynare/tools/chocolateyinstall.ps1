6f78b9a34d77632f0d9defca6a697d31371d6bda
ITS-Unibas <ITS-Unibas-github@gmail.com>
2023-03-22 17:28:27 +0000

    AU: 1 updated - dynare [skip ci]

--- automatic/dynare/tools/chocolateyinstall.ps1
+++ automatic/dynare/tools/chocolateyinstall.ps1
@@ -7,12 +7,12 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://www.dynare.org/release/windows/dynare-5.3-win.exe'
+  url            = 'https://www.dynare.org/release/windows/dynare-5.4-win.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   # optional, highly recommended
   softwareName   = 'Dynare*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '5e84f428ec2ec3e3f7f6b7493d3d7e98079d26098d81be328048d6131178e0eb'
+  checksum       = 'ef5874321e2d3a34ca73ee57ee9e9311c0b07bd4b0a87fbd7faf89dab9fee37f'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
