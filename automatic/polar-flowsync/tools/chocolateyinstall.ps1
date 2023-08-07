ca54a8f2b9c4d6adeb130b4dd04c45c0d65bf592
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2023-08-07 13:03:30 +0200

    updated to version 4.0.11

--- automatic/polar-flowsync/tools/chocolateyinstall.ps1
+++ automatic/polar-flowsync/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = 'https://flowsync.cdn.polar.com/connect/download/FlowSync_3.0.0.1337.exe'
+  url            = 'https://flowsync.flow.polar.com/app/FlowSync_installer-4.0.11-x86-x64.exe'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0, 3010, 1641)
   softwareName   = 'Polar FlowSync*'
-  checksum       = '404631183cb271e45bca369a11103a012c44718e94ca701d4a4976111176513c'
+  checksum       = '0b24c41eb404ef8e9b1963b852d5dfa907f99d8cf1789cdf55f5c45370bc9abf'
   checksumType   = 'sha256'
 }
 
