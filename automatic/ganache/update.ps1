1cfc754741825de4523de9fb668e652628421462
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2023-08-07 10:48:11 +0200

    corrected $matching to find installer

--- automatic/ganache/update.ps1
+++ automatic/ganache/update.ps1
@@ -1,8 +1,9 @@
 Import-Module AU
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$releases = 'https://api.github.com/repos/trufflesuite/ganache-ui/releases'
-$matching = 'win-setup.exe$'
+
+$apiEndpoint = "https://api.github.com"
+$releases = "$apiEndpoint/repos/trufflesuite/ganache-ui/releases"
 
 function global:au_BeforeUpdate() {
   Get-RemoteFiles -Purge -FileNameBase 'ganache'
@@ -20,10 +21,15 @@ function global:au_GetLatest {
   $Splat = @{
       Method      = 'GET'
       Uri         = $releases
+      Headers     = @{
+        "Accept" = "application/vnd.github.v3+json" 
+      }
   }
+  
   $download_page = Invoke-RestMethod @Splat -ErrorAction Stop
-  $results = ($download_page | Where {$_.prerelease -eq $false})[0]
-  $url = $results.assets.browser_download_url | Where {$_ -match $matching}
+  $results = ($download_page | Where-Object {$_.prerelease -eq $false})[0]
+  $matching = 'win-x64-setup.exe$'
+  $url = $results.assets.browser_download_url | Where-Object {$_ -match $matching}
 
   $arr = $url -split '-|.exe'
   $version = $arr[2]
