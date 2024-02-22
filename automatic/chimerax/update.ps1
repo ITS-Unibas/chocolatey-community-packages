a5e7dce2da0958405146721445323c91d99bb2a7
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-02-22 13:56:36 +0100

    Create update.ps1

---
+++ automatic/chimerax/update.ps1
@@ -0,0 +1,38 @@
+import-module au
+
+function global:au_GetLatest {
+    $BaseURL = 'https://www.cgl.ucsf.edu'
+    $chimeraXPage = $BaseURL + '/chimerax/data/release-info/production.json'
+
+    $downloadPage = Invoke-WebRequest -Uri $chimeraXPage
+    $json = $downloadPage.Content | ConvertFrom-Json
+    $version = $json.windows.version
+    $link = $json.windows.link
+
+    $URI = $BaseURL + '/chimerax/cgi-bin/secure/chimerax-get.py'
+   
+    $body = @{
+        'choice' = 'Accept'
+        'file'   = $link 
+    }
+
+    $result = Invoke-WebRequest $URI -body $body -Method Post
+
+    $url64 = $BaseURL + ($result.links | Select-Object -ExpandProperty href)
+
+    return @{ 
+        Version = $version
+        URL64 = $url64 -replace '&amp;','&'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]AppVersion = )('.*')"      = "`$1'$($Latest.Version)'"
+            "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update -ChecksumFor 64
