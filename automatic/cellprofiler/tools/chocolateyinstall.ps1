05dc623a9fdb77b2133a833af74bf265ee680648
ITS-Unibas <ITS-Unibas-github@gmail.com>
2024-07-30 04:40:45 +0000

    AU: 1 updated - cellprofiler [skip ci]

--- automatic/cellprofiler/tools/chocolateyinstall.ps1
+++ automatic/cellprofiler/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = 'https://cellprofiler-releases.s3.amazonaws.com/CellProfiler-Windows-4.2.6.exe'
+  url            = 'https://cellprofiler-releases.s3.amazonaws.com/CellProfiler-Windows-4.2.7.exe'
   silentArgs     = '/VERYSILENT /NORESTART /NOCANCEL'
   validExitCodes = @(0)
   softwareName   = 'Cellprofiler*'
-  checksum       = '723c76d0e431782be3c6f8098a77f4a51bb4d0400fb322656f1f8c8df9091892'
+  checksum       = '42e05146bae68b4f4bea10a94c88786285b9df0f216c6e2bd1ce09bfb09b1df8'
   checksumType   = 'sha256'
 }
 
