b9e3003222d2983ef15fdfa556a92ee3f51d0535
ITS-Unibas <ITS-Unibas-github@gmail.com>
2025-10-21 14:38:53 +0000

    AU: 1 updated - atlassian-companion [skip ci]

--- automatic/atlassian-companion/tools/chocolateyinstall.ps1
+++ automatic/atlassian-companion/tools/chocolateyinstall.ps1
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes = @(0, 3010, 1641)
   #OTHERS
   softwareName   = 'atlassian-companion*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum       = '0ff614cc0a608787dc4f56b8d47732c9bcd07065d36f3a2fd36f5a200e3b7c2f'
+  checksum       = 'f86b6e5e76a1795b5ab587cff2bc3af2e05c7aaf2c0d501bb8c2909f89449cf2'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
