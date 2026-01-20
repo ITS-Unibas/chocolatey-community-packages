64ccfb580b4459a6ff0f4f57fce489a43321a2f3
Marius Lengwiler <marius.lengwiler@unibas.ch>
2026-01-20 16:26:00 +0100

    Update chocolateyinstall.ps1

--- automatic/chimerax/tools/chocolateyinstall.ps1
+++ automatic/chimerax/tools/chocolateyinstall.ps1
@@ -1,32 +1,14 @@
-﻿$ErrorActionPreference = 'Stop'
-
-$AppVersion = '1.11'
-$AppVersionShort = $AppVersion -replace '.\d$'
-
-$BaseURL = 'https://www.cgl.ucsf.edu'
-
-# Discovering download link
-$url = "$BaseURL/chimerax/cgi-bin/secure/chimerax-get.py"
-$PostParams = @{
-   choice='Accept'
-   file="$AppVersionShort/windows/ChimeraX-$AppVersion.exe"
-}
-$DownloadPage = Invoke-WebRequest $url -method Post -Body $postparams -UseBasicParsing
-
-$URLstub = ($DownloadPage.content.split() | Where-Object {$_ -match 'href='}) -replace '.*href="(.*)".*','$1'
-
-Write-Host 'You are establishing a license agreement as defined here:' -ForegroundColor Cyan
-Write-Host 'http://www.cgl.ucsf.edu/chimerax/license.html' -ForegroundColor Cyan
+$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-   packageName    = $env:ChocolateyPackageName
-   fileType       = 'EXE'
-   url64bit       = $BaseURL + $URLstub
-   softwareName   = 'UCSF ChimeraX*' 
-   checksum64     = ''
-   checksumType64 = 'sha256'
-   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-   validExitCodes = @(0)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64            = ''
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  softwareName   = 'UCSF ChimeraX*'
+  checksum64       = ''
+  checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
 Install-ChocolateyPackage @packageArgs
