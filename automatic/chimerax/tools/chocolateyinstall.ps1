9614c09e660be999c302db64a1c464c2dae2ddfb
ITS-Unibas <ITS-Unibas-github@gmail.com>
2026-01-24 04:51:20 +0000

    updated chocolatey-community-packages

--- automatic/chimerax/tools/chocolateyinstall.ps1
+++ automatic/chimerax/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
-  url64            = 'https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py?ident=OHeQer2WTaZn%2F%2BVotntA5eBouVBVQtjx1xp53wvhnvYoo%2BrKciE%3D&file=1.11%2Fwindows%2FChimeraX-1.11.exe&choice=Notified'
+  url64            = 'https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py?ident=OHeQer2WTaZn%2F%2BputnhL4%2BBpv1BVQtjx1Rlx1A%2FhnvEpqe7EdSI%3D&file=1.11.1%2Fwindows%2FChimeraX-1.11.1.exe&choice=Notified'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
   softwareName   = 'UCSF ChimeraX*'
-  checksum64       = '46d79196b375c63b26e3604b358b9cb9d8fdfa292902aeb07e03eb508e3b29c0'
+  checksum64       = 'fd165b6fd3ee17e034d126d9bb2b29fc83be8e9a3ee860c8d74d9018634a7294'
   checksumType   = 'sha256' #default is md5, can also be sha1
 }
 
